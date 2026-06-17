ac1c463b6bc77949a1210cf5620112286ffefe2f
Juliana Aguirre <julianaguirre143@gmail.com>
2026-06-17 11:05:06 -0300

    arreglos finales

--- calculadora.js
+++ calculadora.js
@@ -1,6 +1,6 @@
 function calcularEdad(anioNacimiento) {
     const anio = parseInt(anioNacimiento);
-    if (isNaN(anio) || anio < 1900 || anio > new Date().getFullYear()) return null;
+    if (isNaN(anio) || anio < 1920 || anio > new Date().getFullYear()) return null;
 
     return new Date().getFullYear() - anio;
 }
